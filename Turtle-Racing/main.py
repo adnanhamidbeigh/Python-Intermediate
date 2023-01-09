954814ebdbe423a7810383510fa6450712aca21a
Adnan Hamid <adnanhamidbeg@gmail.com>
2023-01-09 22:20:05 +0530

    Turtles race

--- Turtle-Racing/main.py
+++ Turtle-Racing/main.py
@@ -8,14 +8,12 @@ y_positions = ["-70", "-40", "-10", "20", "50", "80"]
 all_turtles = []
 is_running = False
 
-
 for turtle_index in range(0, 6):   
     new_turtle = Turtle(shape="turtle")
     new_turtle.color(colours[turtle_index])
     new_turtle.penup()
     new_turtle.goto(x=-230, y=float(y_positions[turtle_index]))
     all_turtles.append(new_turtle)
-
 
 if user_bet:
     is_running = True
@@ -32,7 +30,7 @@ while is_running:
                 print(f"You've won! The {winning_colour} turtle is the winner!")
             else:
                 print(f"You've lost! The {winning_colour} turtle is the winner!")
-                
+
         rand_distance = random.randint(0, 10)
         turtle.fd(rand_distance)
 
